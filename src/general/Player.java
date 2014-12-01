0522ccdc634f0ae96bcdce3454361f76cc979d03
Neumaier Moritz <NeumaierMoritz@neumaiers-mbp.speedport_w_921v_1_36_000>
2014-12-01 11:31:50 +0100

    player

--- src/general/Player.java
+++ src/general/Player.java
@@ -1,5 +1,19 @@
 package general;
 
 public class Player {
-
+	String name;
+	
+	public Player(String name){
+		this.name = name;
+		
+		Field ownField = new Field();
+		Field enemyField = new Field();
+	}
+	
+	private void setShip(){
+		
+	}
+	private void shoot(){
+		
+	}
 }
