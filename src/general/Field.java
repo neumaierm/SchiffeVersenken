5f5ae31149eb7e71e3b53a2b2182285e62270408
Johannes <joekoe92@gmail.com>
2014-12-01 11:29:02 +0100

    added bin to gitignore; foundation of field

--- src/general/Field.java
+++ src/general/Field.java
@@ -1,5 +1,19 @@
 package general;
 
+import java.util.LinkedList;
+
 public class Field {
+	private final int size = 64;
+	private Block[] myBlocks;
+	private LinkedList<Ship> ships;
+	
+	public Field() {
+		myBlocks = new Block[size];
+		ships = new LinkedList<>();
+	}
+	
+	public LinkedList<Ship> getShips(){
+		return ships;
+	}
 
 }
